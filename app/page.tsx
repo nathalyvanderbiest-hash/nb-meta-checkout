99a3a7f33b2e88518c2c7514a3e96f2f58b62a1e
nathalyvanderbiest-hash <nathalyvanderbiest@gmail.com>
2026-07-01 16:22:27 -0400

    Create page.tsx
    step 2

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: 40 }}>
+      <h1>Nathaly Botanicals Meta Checkout</h1>
+      <p>The checkout service is running.</p>
+    </main>
+  );
+}
